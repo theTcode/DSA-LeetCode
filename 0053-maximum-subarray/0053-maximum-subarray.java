bcf5aef3376d156f16293582942783f5bca108f6
Tarun Daderwal <112380834+theTcode@users.noreply.github.com>
2024-09-05 00:23:50 +0530

    Time: 1 ms (99.48%), Space: 56.8 MB (73.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,16 +3,11 @@ class Solution {
         int maxi = Integer.MIN_VALUE;
         int sum = 0;
         for(int i=0;i<nums.length;i++){
-            sum += nums[i];
-
             if(sum < 0){
                 sum=0;
             }
+            sum += nums[i];
             maxi = Math.max(maxi, sum);
-        }
-        if(maxi==0){
-            Arrays.sort(nums);
-            maxi = nums[nums.length-1];
         }
         return maxi;
     }
