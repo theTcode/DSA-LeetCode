5f4eb16224ef88bc000beb35a795985c4ccc7978
Tarun Daderwal <112380834+theTcode@users.noreply.github.com>
2024-09-07 23:01:02 +0530

    Time: 0 ms (100%), Space: 41.3 MB (65.61%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int max=0, min=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                max=max+1;
+                min=min+1;
+            }
+            else if(s.charAt(i)==')'){
+                min=min-1;
+                max=max-1;
+            }
+            else{
+                min=min-1;
+                max=max+1;
+            }
+            if(min<0){
+                min=0;
+            }
+            if(max<0){
+                return false;
+            }
+        }
+        return (min==0);
+    }
+}
