53275c19066632e1713cbd1d5e7f8490d8d1efb8
Tarun Daderwal <112380834+theTcode@users.noreply.github.com>
2024-09-18 00:59:40 +0530

    Update 1250-longest-common-subsequence.java
    Added a different approach

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,9 +1,12 @@
 class Solution {
+    //Approach-1
     public int longestCommonSubsequence(String text1, String text2) {
         int i = text1.length();
         int j = text2.length();
         int ans = 0;
         int[][] dp = new int[i+1][j+1];
+        // We can remove these two loops because, in Java, the default value of elements in an array is 0.
+        // So we can directly start from row=1 & col=1.
         // for(int r=0;r<i;r++){
         //    dp[r][0]=0;
         // }
@@ -22,5 +25,32 @@ class Solution {
             }
         }
         return dp[i][j];
+    }
+
+    //Approach-2
+    public int longestCommonSubsequence(String text1, String text2) {
+        int i = text1.length();
+        int j = text2.length();
+        int[][] dp = new int[i][j];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        return lcs(i-1, j-1, text1, text2, 0, dp);
+        
+    }
+    private int lcs(int i, int j, String s1, String s2, int ans, int[][] dp){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1.charAt(i) == s2.charAt(j)){
+            ans = 1 + lcs(i-1, j-1, s1, s2, ans, dp);
+        }
+        else{
+            ans = Math.max(lcs(i, j-1, s1, s2, ans, dp), lcs(i-1, j, s1, s2, ans, dp));
+        }
+        return dp[i][j] = ans;
     }
 }
