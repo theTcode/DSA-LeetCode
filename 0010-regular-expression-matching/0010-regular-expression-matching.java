2f359bf6906a02fb1eed65709240996f22543254
Tarun Daderwal <112380834+theTcode@users.noreply.github.com>
2024-09-24 03:29:10 +0530

    Time: 2 ms (64.49%), Space: 42 MB (53.33%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -16,13 +16,16 @@ class Solution {
                     dp[n][m] = dp[n-1][m-1];
                 }
                 else if(p.charAt(m-1) == '*'){
-                    dp[n][m] = (dp[n][m-2] || (match(s, p, n, m) && dp[n-1][m]));
+                    dp[n][m] = dp[n][m-2];
+                    if(s.charAt(n-1) == p.charAt(m-2) || p.charAt(m-2) == '.'){
+                        dp[n][m] = dp[n][m] || dp[n-1][m];
+                    }
+                }
+                else{
+                    dp[n][m] = false;
                 }
             }
         }
         return dp[i][j];
-    }
-    private boolean match(String s, String p, int i, int j){
-        return (s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.');
     }
 }
