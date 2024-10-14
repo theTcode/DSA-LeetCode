a13858eb2731d0cbf8360f61b48c10ddcf98d9bd
Tarun Daderwal <112380834+theTcode@users.noreply.github.com>
2024-10-15 03:00:14 +0530

    Time: 31 ms (97.97%), Space: 42.7 MB (31.28%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -9,25 +9,35 @@ class Solution {
         }
         coin[n+1]=1;
 
-        int[][] dp = new int[n+1][n+1];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
-        }
+        int[][] dp = new int[n+2][n+2];
 
-        return sum(1, n, coin, dp);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    continue;
+                }
+                int maxi = Integer.MIN_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int cost = coin[i-1]*coin[ind]*coin[j+1] + dp[i][ind-1] + dp[ind+1][j];
+                    maxi = Math.max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
     }
-    public int sum(int i, int j, int[] nums, int[][] dp){
-        if(i>j){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int maxi = Integer.MIN_VALUE;
-        for(int ind=i;ind<=j;ind++){
-            int cost = nums[i-1]*nums[ind]*nums[j+1] + sum(i, ind-1, nums, dp) + sum(ind+1, j, nums, dp);
-            maxi = Math.max(maxi, cost);
-        }
-        return dp[i][j] = maxi;
-    }
+    // public int sum(int i, int j, int[] nums, int[][] dp){
+    //     if(i>j){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int maxi = Integer.MIN_VALUE;
+    //     for(int ind=i;ind<=j;ind++){
+    //         int cost = nums[i-1]*nums[ind]*nums[j+1] + sum(i, ind-1, nums, dp) + sum(ind+1, j, nums, dp);
+    //         maxi = Math.max(maxi, cost);
+    //     }
+    //     return dp[i][j] = maxi;
+    // }
 }
